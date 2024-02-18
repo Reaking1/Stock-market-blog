49d379e12cb3dab5672f6dabe1a8dcc1140699dc
Reaking1 <reabetsweramoswane@gmail.com>
2024-02-18 13:05:41 +0200

    stockblog updated

---
+++ Stockblog/src/api/apiService.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+
+const API_KEY = 'IBLCVK3BICIVSYJR';
+const BASE_URL = 'https://www.alphavantage.co';
+
+export const ApiService = {
+    fetchStockData: async (symbol: string) => {
+        const url = `${BASE_URL}/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`;
+        const res = await axios.get(url);
+        return res.data
+    },
+
+    fetchGlobalNews: async () => {
+        const url = `${BASE_URL}/query?function=LIST&keywords=global news&apikey=${API_KEY}`;
+        const res = await axios.get(url);
+        return res.data
+    },
+
+    fetchStockDataWeekly: async (symbol: string) => {
+        const url = `${BASE_URL}/query?function=TIME_SERIES_WEEKLY&symbol=${symbol}&apikey=${API_KEY}`;
+         const res = await axios.get(url)
+         return res.data;
+    },
+
+    fetchStockMonthly: async (symbol: string) => {
+        const url = `${BASE_URL}/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${API_KEY}`;
+        const res = await axios.get(url)
+        return res.data
+    },
+
+    fetchStockHistory: async (symbol: string, timeframe: string) => {
+        let functionName = '';
+        switch (timeframe) {
+            case 'daily': 
+            functionName = 'TIME_SERIES_DAILY';
+            break
+            case 'weekly': 
+            functionName = 'TIME_SERIES_WEEKLY';
+            break
+            case 'monthly': 
+            functionName = 'TIME_SERIES_MONTHLY';
+            break
+            default:
+                throw new Error('Invaild timeframe')
+        }
+        const url = `${BASE_URL}/query?function=${functionName}&symbol=${symbol}&apikey=${API_KEY}`;
+        const res = await axios.get(url)
+        return res.data
+    },
+
+    fetchExchangeRates: async () => {
+         const url = `${BASE_URL}/query?function=LIST&keywords=global news&apikey=${API_KEY}`;
+         const res = await axios.get(url);
+         return res.data
+    }
+
+ 
+}
--- Stockblog/src/components/StockPage.tsx
+++ Stockblog/src/components/StockPage.tsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { ApiService } from '../api/apiService';
+import { useParams } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
+
+interface StockPage {
+    name: string,
+    price: number,
+}
 
 
 const StockPage: React.FC = () => {
-    const {symbol} = useParams<{ symbol: string}>();
+    const { symbol } = useParams<{ symbol?: string }>();
+  
+ const [stockData, setStockData] = useState<StockPage | null>(null);
+ const [stockHistory, setStockHistory] = useState<number[]>([])
+  const data = {
+    labels: ['1', '2', '3', '4', '5', '6', '7'],
+    datasets: [
+        {
+            label: 'Stock Price',
+            data: stockHistory,
+            fill: false,
+            backgroundColor: 'rgb(75, 192, 192)',
+            borderColor: 'rgba(75, 192, 192, 0.2)',
+        }
+    ]
+  }
+
+   useEffect(() => {
+    if(symbol) {
+    const fetchData = async () => {
+        const data = await ApiService.fetchStockData(symbol);
+        setStockData(data)
+    };
+    fetchData()
+}
+   }, [symbol])
+
+
+   useEffect(() => {
+    if (symbol) {
+        const fetchHistroy = async () => {
+            const histroy = await ApiService.fetchStockHistory(symbol);
+            setStockHistory(histroy);
+        };
+
+        fetchHistroy()
+    }
+},
+[symbol])
 
     return (
         <div>
             <h1>Stock Page</h1>
             <p>Symbol: {symbol}</p>
             {/** Fetch data from the stock market for a specific symbol */}
+            {stockData && (
+                <div>
+                    <p>Name: {stockData.name}</p>
+                    <p>Price: {stockData.price}</p>
+                </div>
+            )}
+            {stockHistory.length > 0 && (
+                 <div className="stock">
+                    <Line data={data} />
+                 </div>
+            )}
+            
         </div>
     )
 }
