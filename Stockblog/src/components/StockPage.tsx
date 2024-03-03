0dce30e2bbfaa7e55fefe95cd5e011e79380d446
Reaking1 <reabetsweramoswane@gmail.com>
2024-03-03 19:56:29 +0200

    updated

--- Stockblog/src/components/StockPage.tsx
+++ Stockblog/src/components/StockPage.tsx
@@ -80,24 +80,52 @@ useEffect(() => {
             <p>Symbol: {symbol}</p>
             {stockData && (
                 <div>
-                    <p>Da: {stockData.name} </p>
+                    <p>Date: {stockData.name} </p>
                     <p>Price: {stockData.price} </p>
                 </div>
             )}
+            <h1>Stock data</h1>
             {stockHistory.length > 0  && (
                 <div className='stock'>
+                  <h2>Historical Stock Data</h2>
+                  <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Open</th>
+                            <th>High</th>
+                            <th>Low</th>
+                            <th>Close</th>
+                            <th>Volume</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stockHistory.map(item => (
+                            <tr key={item.date}>
+                                <td>{item.date}</td>
+                                <td>{item.open}</td>
+                                <td>{item.high}</td>
+                                <td>{item.low}</td>
+                                <td>{item.close}</td>
+                                <td>{item.volume}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                  <div className="chart">
                     <Line data={{
-                      labels: stockHistory.map(item => item.date),
-                      datasets: [
-                          {
-                              label: 'Stock Price',
-                              data: stockHistory.map(item => item.close),
-                              fill: false,
-                              backgroundColor: 'rgb(75, 192, 192)',
-                              borderColor: 'rgba(75, 192, 192, 0.2)',
-                          },
-                      ],
-                    }}/>
+                        labels: stockHistory.map(item => item.date),
+                        datasets: [
+                            {
+                                label: 'Stock Price',
+                                data: stockHistory.map(item => item.close),
+                                fill: false,
+                                backgroundColor: 'rgb(75,92, 193)',
+                                borderColor: 'rgba(75, 192, 192,0.2)'
+                            }
+                        ]
+                    }} />
+                  </div>
                 </div>
             )}
         </div>
